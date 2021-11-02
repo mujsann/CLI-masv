c702e3b4ebb7893858849be0f2558cb6ddcfc957
Gboye <mujsann@gmail.com>
2021-11-02 15:09:07 -0400

    updated readme and inputs

--- README.md
+++ README.md
@@ -1,7 +1,19 @@
+## Description
+A command-line program that:
+
+Accepts any number of parameters as command line arguments.
+
+Each parameter should be of the form <number><string> where <number> is a single digit from 0 to 9 and <string> is a lower case string composed of the characters from a to z with length of at least 1.
+
+Each parameter should be processed in the following way with the output from each parameter printed on its own line:
+
+The digit indicates how many character positions each character should be increased by, with wraparound from "z" to "a".
+
+The modified string, without its digit prefix, should be printed to STDOUT.
 
 ## Usage
 
-To run this project, download it or clone it. 
+To run this program, download it or clone it. 
 
 ```bash
 git clone https://www.github.com/mujsann/cli
--- mock-inputs.js
+++ mock-inputs.js
@@ -1,6 +1,6 @@
 
-const mock_inputs = ['0apple', '1lzru', '6gump', '8hello', '5zyo', '3abc']
-const expected_outputs = ['apple', 'masv', 'masv', 'pmttw', 'edt', 'def']
+const mock_inputs = ['0apple', '1lzru', '6gump', '8hello', '5zyo', '3abc', '4abc', '1xyz', '0mnp']
+const expected_outputs = ['apple', 'masv', 'masv', 'pmttw', 'edt', 'def', 'efg', 'yza', 'mnp' ]
 
 const mock_inputs2 = ['0apple', 'l1zru', '6gump', 'he8llo', '5zyo', '3abc']
 const expected_outputs2 = ['apple', "the first character should be a number in 'l1zru'", 'masv', "the first character should be a number in 'he8llo'", 'edt', 'def']
@@ -10,4 +10,6 @@ const expected_output3 = ['apple', 'masv', "the string should only contain chara
 
 
 const data = { mock_inputs, expected_outputs, mock_inputs2, expected_outputs2, mock_inputs3, expected_output3 }
+
+
 module.exports = data
